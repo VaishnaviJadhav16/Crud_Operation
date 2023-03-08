937c40253fbcb53512e4184f3b29448c0e21fb40
VaishnaviJadhav16 <jadhavvaishnavi16@gmail.com>
2023-03-08 16:33:08 +0530

    crud

---
+++ Application_crud/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Application_crud.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required]
+        [DisplayName("EMP ID")]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("ProductID")]
+        public int ProductID { get; set; }
+        [Required]
+        [DisplayName("ProductName")]
+        public string ProductName { get; set; }
+        [Required]
+        [DisplayName("CategoryName")]
+        public string CategoryName { get; set; }
+        [Required]
+        [DisplayName("CategoryID")]
+        public int CategoryID { get; set; }
+
+    }
+}
